0e1f15b366955b23fdf7c5141b63d34cce252dda
KevinGarciaCortez <nike_0316@hotmail.com>
2022-06-06 10:53:30 -0700

    1

---
+++ EscrituraByte/EscrituraByte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraByte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nbyte=0,car;
+            try
+            {
+                fs = new FileStream("text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escriba:");
+                while ((car = Console.Read()) != '\r' && (nbyte < buffer.Length))
+                {
+                    buffer[nbyte] = (byte)car;
+                    nbyte++;
+                }
+                fs.Write(buffer, 0, nbyte);
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(fs!=null)fs.Close();
+            }
+        }
+    }
+}
